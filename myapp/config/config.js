caf1bdfb60ce418044c8f9f70fa4b0cf7bd10479
atemp678 <ajt2158@columbia.edu>
2022-01-19 10:53:46 +0000

    tried deployment

---
+++ myapp/config/config.js
@@ -0,0 +1,7 @@
+export const db = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+};
