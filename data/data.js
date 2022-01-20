b8e527c2d3707b683f955f940f21ca83f3bd1a05
atemp678 <ajt2158@columbia.edu>
2022-01-20 13:58:03 +0000

    changed embed

--- data/data.js
+++ data/data.js
@@ -3,25 +3,25 @@ const links = [
     id: 1,
     title: "JavaScript in 5 min",
     topic: "Javascript",
-    URL: "https://www.youtube.com/watch?v=c-I5S_zTwAc",
+    URL: "https://www.youtube.com/embed/c-I5S_zTwAc",
   },
   {
     id: 2,
     title: "Learn HTML in 12 Minutes",
     topic: "HTML",
-    URL: "https://www.youtube.com/watch?v=bWPMSSsVdPk",
+    URL: "https://www.youtube.com/embed/bWPMSSsVdPk",
   },
   {
     id: 3,
     title: "Learn CSS in 20 Minutes",
     topic: "CSS",
-    URL: "https://www.youtube.com/watch?v=1PnVor36_40",
+    URL: "https://www.youtube.com/embed/1PnVor36_40",
   },
   {
     id: 4,
     title: "React in 100 Seconds",
     topic: "React",
-    URL: "https://www.youtube.com/watch?v=Tn6-PIqc4UM",
+    URL: "https://www.youtube.com/embed/Tn6-PIqc4UM",
   },
 ];
 export default links;
